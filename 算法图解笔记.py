8e7fc5c20c6ed9eabd73947fdbebd8f82f30ff48
yaqi <60095876+yaqitian@users.noreply.github.com>
2022-04-19 13:04:37 +0200

    Update and rename 算法图解.md to 算法图解笔记.py

--- 算法图解笔记.py
+++ 算法图解笔记.py
@@ -1,5 +1,6 @@
 # 第一章 二分查找
-``` python
+
+# 基础二分查找
 def binary_search(list, item):
     low = 0
     high = len(list) - 1
@@ -19,9 +20,9 @@ if __name__ == '__main__':
     my_list = [1, 3, 5, 7, 9]
     print(binary_search(my_list, 3))  # => 1
     print(binary_search(my_list, -1))  # => None
-```
-练习<br>
-1.1 假设有一个包含128个名字的有序列表，你要使用二分查找在其中查找一个名字，请问最多需要几步才能找到？<br>
-&emsp;log 128 = 7<br>
-1.2 上面列表的长度翻倍后，最多需要几步？<br>
-&emsp;log 256 = 8<br>
+
+# 练习
+# 1.1 假设有一个包含128个名字的有序列表，你要使用二分查找在其中查找一个名字，请问最多需要几步才能找到？
+#     log 128 = 7
+# 1.2 上面列表的长度翻倍后，最多需要几步？
+#     log 256 = 8
