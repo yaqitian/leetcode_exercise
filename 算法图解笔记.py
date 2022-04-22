a4800dd41fd0a5ac3630cfe1ed27a637a8a2b774
yaqi <60095876+yaqitian@users.noreply.github.com>
2022-04-22 10:57:41 +0200

    Update 算法图解笔记.py

--- 算法图解笔记.py
+++ 算法图解笔记.py
@@ -93,4 +93,79 @@ def quicksort(array):
         pivot = array[0] 
         less = [i for i in array[1:] if i <= pivot] 
         greater = [i for i in array[1:] if i > pivot] 
-        return quicksort(less) + [pivot] + quicksort(greater) 
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+
+
+# 第七章 狄克斯特拉算法
+
+def find_lowest_cost_node(costs, processed):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs.keys():
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+def prepare_1():
+    # Initialize graph.
+    graph = {}
+    graph["Start"] = {}
+    graph["Start"]["a"] = 5
+    graph["Start"]["b"] = 2
+
+    graph["a"] = {}
+    graph["a"]["c"] = 4
+    graph["a"]["d"] = 2
+
+    graph["b"] = {}
+    graph["b"]["a"] = 8
+    graph["b"]["d"] = 7
+
+    graph["c"] = {}
+    graph["c"]["Final"] = 3
+    graph["c"]["d"] = 6
+
+    graph["d"] = {}
+    graph["d"]["Final"] = 1
+
+    graph["Final"] = {}
+
+    # Initialize costs.
+    infinity = float("inf")
+    costs = {}
+    costs["a"] = 5
+    costs["b"] = 2
+    costs["c"] = infinity
+    costs["d"] = infinity
+    costs["Final"] = infinity
+
+    # Initialize parents.
+    parents = {}
+    parents["a"] = "Start"
+    parents["b"] = "Start"
+    parents["c"] = None
+    parents["d"] = None
+    parents["Final"] = None
+
+    return graph, costs, parents
+
+def solution_71():
+    graph, costs, parents = prepare_1()
+
+    processed = []
+    node = find_lowest_cost_node(costs, processed)
+    while node is not None:
+        cost = costs[node]
+        neighbors = graph[node]
+        for n in neighbors.keys():
+            new_cost = cost + neighbors[n]
+            if costs[n] > new_cost:
+                costs[n] = new_cost
+                parents[n] = node
+        processed.append(node)
+        node = find_lowest_cost_node(costs, processed)
+
+    return costs["Final"]
